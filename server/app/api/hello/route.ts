66627cf7134f420b22e2bf84673adb1bdce90828
EPham42747 <ethancpham@gmail.com>
2025-07-23 14:57:07 -0400

    Deployment fix 5

--- server/app/api/hello/route.ts
+++ server/app/api/hello/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default function GET() {
+export default function GET(req: NextRequest) {
   return NextResponse.json({ message: 'Hello world!' }, { status: 200 });
 }
