9a4a03e9bb158c8b79800da2cf2d251699d33c25
Daniel Kim <123012677+D-SehKim@users.noreply.github.com>
2025-07-23 15:55:31 -0400

    Began Mobile Frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <nav>
-        <a href="/food/">FOOD</a>
-      </nav>
-      hello
+      <div className="bg-umass-red text-white top-1/20 left-5/100 w-9/10 min-w-50 h-15 rounded-full absolute drop-shadow-lg/50 flex gap-2">
+        <button className="text-white right-1/100 w-25 h-15 px-2">ham</button>
+        <button className="text-white w-5/5 h-15 px-2">SEARCH</button>
+        <button className="text-white w-25 h-15 px-2">PFP</button>
+      </div>
+      <button className="bg-umass-red text-4xl text-white bottom-1/30 right-5/100 w-15 h-15 rounded-full absolute drop-shadow-lg/50">
+        +
+      </button>
     </div>
   );
 }
