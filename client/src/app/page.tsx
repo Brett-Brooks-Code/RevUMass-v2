eef0068ee66f2cb0a7f343e322cbc6936599d346
Daniel Kim <123012677+D-SehKim@users.noreply.github.com>
2025-07-23 17:01:03 -0400

    added button functionality

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,38 @@
+'use client';
+
 export default function Home() {
+
+  function handleSearchClick() {
+    console.log('search')
+  }
+
+  function handleMenuClick() {
+    console.log('Menu')
+  }
+
+  function handlePfpClick() {
+    console.log('pfp')
+  }
+
+  function handleAddClick() {
+    console.log('add')
+  }
+
   return (
     <div>
+      {/* appbar */}
       <div className="bg-umass-red text-white top-1/20 left-5/100 w-9/10 min-w-50 h-15 rounded-full absolute drop-shadow-lg/50 flex gap-2">
-        <button className="text-white right-1/100 w-25 h-15 px-2">ham</button>
-        <button className="text-white w-5/5 h-15 px-2">SEARCH</button>
-        <button className="text-white w-25 h-15 px-2">PFP</button>
+        <button className="text-white right-1/100 w-25 h-15 px-2" onClick={handleMenuClick} >ham</button>
+        <button className="text-white w-5/5 h-15 px-2" onClick={handleSearchClick}>SEARCH</button>
+        <button className="text-white w-25 h-15 px-2" onClick={handlePfpClick}>PFP</button>
       </div>
-      <button className="bg-umass-red text-4xl text-white bottom-1/30 right-5/100 w-15 h-15 rounded-full absolute drop-shadow-lg/50">
+      {/* add button */} 
+      <button className="bg-umass-red text-4xl text-white bottom-1/30 right-5/100 w-15 h-15 rounded-full absolute drop-shadow-lg/50" onClick={handleAddClick}>
         +
+      </button>
+      {/* add button */} 
+      <button>
+
       </button>
     </div>
   );
