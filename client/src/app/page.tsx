4f8eebda57134405da427e22bfab1d059b0d13f8
Daniel Kim <123012677+D-SehKim@users.noreply.github.com>
2025-08-11 18:06:15 -0400

    addedd button

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -45,6 +45,13 @@ export default  function Home() {
                 <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-red-700 hover:bg-grey-900 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Search</button>
             </div>
         </form>
+        <button className="Btn relative w-3/5 left-1/5 top-5/20 h-1/10">
+          <div className="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
+          <div className="text">Write a Review</div>
+        </button>
+
+
+
       </div>
       <div className="h-screen">
         {/* appbar */}
